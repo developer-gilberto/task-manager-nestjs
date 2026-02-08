a29645881e06217b818789e81fb32f66ceb09648
gilberto <developer.gilberto@gmail.com>
2026-02-08 16:54:24 -0300

    feat: create task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service'
 import { AppController2 } from './app2.controller'
 import { ProjectsModule } from './modules/projects/projects.module'
 import { PrismaService } from './prisma.service'
+import { TasksModule } from './modules/tasks/tasks.module';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, TasksModule],
   controllers: [AppController, AppController2],
   providers: [AppService, PrismaService],
 })
