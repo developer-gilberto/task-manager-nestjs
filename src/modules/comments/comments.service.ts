9920fa49816c14802f836e41884eb452a28cada3
gilberto <developer.gilberto@gmail.com>
2026-02-12 16:07:47 -0300

    feat: create services for comments

--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,4 +1,111 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { CommentRequestDTO } from './comments.dto'
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(private readonly PrismaClient: PrismaService) {}
+
+  async getAllByTask(task_id: string) {
+    return await this.PrismaClient.comment.findMany({
+      where: { task_id },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true,
+          },
+        },
+      },
+    })
+  }
+
+  async getById(task_id: string, comment_id: string) {
+    return await this.PrismaClient.comment.findFirst({
+      where: {
+        id: comment_id,
+        task_id,
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true,
+          },
+        },
+        task: {
+          select: {
+            id: true,
+            title: true,
+            project_id: true,
+          },
+        },
+      },
+    })
+  }
+
+  async create(task_id: string, data: CommentRequestDTO) {
+    return await this.PrismaClient.comment.create({
+      data: {
+        content: data.content,
+        task_id,
+        author_id: '123', // MUDAR ID DO USUARIO
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true,
+          },
+        },
+      },
+    })
+  }
+
+  async update(task_id: string, comment_id: string, data: CommentRequestDTO) {
+    const comment = await this.PrismaClient.comment.findFirst({
+      where: {
+        id: comment_id,
+        task_id,
+      },
+    })
+
+    if (!comment) throw new NotFoundException('Comment not found')
+
+    return await this.PrismaClient.comment.update({
+      where: { id: comment_id },
+      data,
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true,
+          },
+        },
+      },
+    })
+  }
+
+  async delete(task_id: string, comment_id: string) {
+    const comment = await this.PrismaClient.comment.findFirst({
+      where: {
+        id: comment_id,
+        task_id,
+      },
+    })
+
+    if (!comment) throw new NotFoundException('Comment not found')
+
+    return await this.PrismaClient.comment.delete({
+      where: { id: comment_id },
+    })
+  }
+}
