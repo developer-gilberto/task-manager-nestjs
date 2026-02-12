bbf0395a97f71cf60de99a597393d7dd190520d3
gilberto <developer.gilberto@gmail.com>
2026-02-12 15:34:48 -0300

    feat: create DTO for comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppControllerV2 } from './app.v2.controller'
+import { CollaboratorsModule } from './modules/collaborators/collaborators.module'
+import { CommentsModule } from './modules/comments/comments.module'
 import { ProjectsModule } from './modules/projects/projects.module'
 import { TasksModule } from './modules/tasks/tasks.module'
 import { UsersModule } from './modules/users/users.module'
 import { PrismaService } from './prisma.service'
-import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
-import { CommentsModule } from './modules/comments/comments.module';
 
 @Module({
   imports: [ProjectsModule, TasksModule, UsersModule, CollaboratorsModule, CommentsModule],
---
+++ src/modules/comments/comments.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString } from 'class-validator'
+
+export class CommentRequestDTO {
+  @ApiProperty({ description: 'Comment content' })
+  @IsString()
+  @IsNotEmpty()
+  content: string
+}
+
+class CommentAuthorDTO {
+  @ApiProperty() id: string
+  @ApiProperty() name: string
+  @ApiProperty() email: string
+  @ApiProperty({ nullable: true }) avatar: string
+}
+
+export class CommentListItemDTO {
+  @ApiProperty() id: string
+  @ApiProperty() content: string
+  @ApiProperty() task_id: string
+  @ApiProperty() author_id: string
+  @ApiProperty({ format: 'date-time' }) created_at: string
+  @ApiProperty({ format: 'date-time' }) updated_at: string
+
+  @ApiProperty({ type: CommentAuthorDTO })
+  author: CommentAuthorDTO
+}
+
+class CommentTaskDTO {
+  @ApiProperty() id: string
+  @ApiProperty() title: string
+  @ApiProperty() project_id: string
+}
+
+export class CommentFullDTO extends CommentListItemDTO {
+  @ApiProperty({ type: CommentTaskDTO })
+  task: CommentTaskDTO
+}
