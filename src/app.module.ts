785ef06333e8d02fab6deb71665a65c8487bf03e
gilberto <developer.gilberto@gmail.com>
2026-02-13 18:17:58 -0300

    feat: create module for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppControllerV2 } from './app.v2.controller'
+import { AuthModule } from './modules/auth/auth.module'
 import { CollaboratorsModule } from './modules/collaborators/collaborators.module'
 import { CommentsModule } from './modules/comments/comments.module'
 import { ProjectsModule } from './modules/projects/projects.module'
@@ -10,7 +11,14 @@ import { UsersModule } from './modules/users/users.module'
 import { PrismaService } from './prisma.service'
 
 @Module({
-  imports: [ProjectsModule, TasksModule, UsersModule, CollaboratorsModule, CommentsModule],
+  imports: [
+    ProjectsModule,
+    TasksModule,
+    UsersModule,
+    CollaboratorsModule,
+    CommentsModule,
+    AuthModule,
+  ],
   controllers: [AppController, AppControllerV2],
   providers: [AppService, PrismaService],
 })
