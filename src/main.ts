604b4c13168dae8d57b0d0dbc3a38a090a47e555
gilberto <developer.gilberto@gmail.com>
2026-02-22 16:09:43 -0300

    feat: configure CORS with environment based origin

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,12 @@ async function bootstrap() {
 
   await app.startAllMicroservices()
 
+  app.enableCors({
+    origin: CONSTANTS.IS_PRODUCTION ? [process.env.FRONTEND_URL] : ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  })
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
   await app.listen(process.env.PORT ?? 3000)
