61fbdcb4d5bfdae9c200a0648257b4baf68de269
gilberto <developer.gilberto@gmail.com>
2026-02-21 22:43:18 -0300

    test: create tests for the JwtAuthGuard guard

--- src/common/guards/jwt-auth/jwt-auth.guard.spec.ts
+++ src/common/guards/jwt-auth/jwt-auth.guard.spec.ts
@@ -1,7 +1,58 @@
-import { JwtAuthGuard } from './jwt-auth.guard';
+import { ExecutionContext } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { RequestContextService } from 'src/common/services/request-context/request-context.service'
+import { JwtAuthGuard } from './jwt-auth.guard'
 
 describe('JwtAuthGuard', () => {
-  it('should be defined', () => {
-    expect(new JwtAuthGuard()).toBeDefined();
-  });
-});
+  let guard: JwtAuthGuard
+  let requestContextService: RequestContextService
+  let mockExecutionContext: ExecutionContext
+
+  beforeEach(() => {
+    requestContextService = {
+      setUser: jest.fn(),
+    } as any
+
+    guard = new JwtAuthGuard(requestContextService)
+
+    mockExecutionContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          user: { id: 1, user_name: 'test-user' },
+        }),
+      }),
+    } as ExecutionContext
+  })
+
+  test('should return true and set user when authentication successfully', async () => {
+    const superCanActivate = jest.spyOn(AuthGuard('jwt').prototype, 'canActivate')
+    superCanActivate.mockResolvedValue(true)
+
+    const result = await guard.canActivate(mockExecutionContext)
+
+    expect(result).toBe(true)
+    expect(requestContextService.setUser).toHaveBeenCalledWith({
+      id: 1,
+      user_name: 'test-user',
+    })
+  })
+
+  test('should return false and not set user when authentication fails', async () => {
+    jest.spyOn(AuthGuard('jwt').prototype, 'canActivate').mockResolvedValue(false)
+
+    const result = await guard.canActivate(mockExecutionContext)
+
+    expect(result).toBe(false)
+    expect(requestContextService.setUser).not.toHaveBeenCalled()
+  })
+
+  test('should throw an error when super.canActivate throws', async () => {
+    const error = new Error('Authentication failed')
+
+    jest.spyOn(AuthGuard('jwt').prototype, 'canActivate').mockRejectedValue(error)
+
+    await expect(guard.canActivate(mockExecutionContext)).rejects.toThrow(error)
+
+    expect(requestContextService.setUser).not.toHaveBeenCalled()
+  })
+})
