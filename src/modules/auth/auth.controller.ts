293d0cbc3ec0b1379e1b5192cdfa315d85c4c279
gilberto <developer.gilberto@gmail.com>
2026-02-22 16:26:42 -0300

    feat: create route /auth/me

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { ApiBearerAuth } from '@nestjs/swagger'
 import { AuthenticatedUser } from 'src/common/decorators/authenticated-user.decorator'
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth/jwt-auth.guard'
 import type { User } from 'src/generated/prisma/client'
+import { UsersService } from '../users/users.service'
 import { ForgotPasswordDTO, ResetPasswordDTO, SignInDTO, SignUpDTO } from './auth.dto'
 import { AuthService } from './auth.service'
 
@@ -11,7 +22,10 @@ import { AuthService } from './auth.service'
   path: 'auth',
 })
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
@@ -30,6 +44,24 @@ export class AuthController {
   @ApiBearerAuth('jwt')
   protected(@AuthenticatedUser() user: User) {
     return { message: `Authenticated! Welcome ${user.name}` }
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('jwt')
+  async me(@AuthenticatedUser() user: User) {
+    const userData = await this.userService.getById(user.id)
+
+    if (!userData) throw new UnauthorizedException('User not found')
+
+    return {
+      id: userData.id,
+      name: userData.name,
+      avatar: userData.avatar,
+      email: userData.email,
+      created_at: userData.created_at,
+      updated_at: userData.updated_at,
+    }
   }
 
   @Post('forgot-password')
